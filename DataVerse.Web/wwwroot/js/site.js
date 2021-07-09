9069ac8d7e802a1387bf2b0d958cdcdd0411c323
vassilas <vasilas.cei@gmail.com>
2021-07-09 11:27:14 +0300

    Move JS code to site.js

--- DataVerse.Web/wwwroot/js/site.js
+++ DataVerse.Web/wwwroot/js/site.js
@@ -3,8 +3,27 @@
 
 // Write your JavaScript code.
 
-jQuery(document).ready(function ($) {
-    $(".clickable-row").click(function () {
-        console.log("Click");
-    });
-});
+
+var CreateCustomerModalShow = function () {
+    $("#CreateModal").modal('show');
+}
+
+var ConfirmDelete = function (CustomerId, CustomerEmail) {
+    $("#hiddenCustomerId").val(CustomerId);
+    $("#hiddenCustomerEmail").val(CustomerEmail);
+    $("#CustomerIdShow").html(CustomerEmail);
+    $("#DeleteModal").modal('show');
+
+}
+var DeleteCustomer = function () {
+    var empId = $("#hiddenCustomerId").val();
+    $.ajax({
+        type: "POST",
+        url: "/Customers/Delete",
+        data: { customer_id: empId },
+        success: function (result) {
+            $("#DeleteModal").modal("hide");
+            $("#row_" + empId).remove();
+        }
+    })
+}
